7367400d21a8b56d175f6e79184ac712bae5ae52
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-05-09 17:14:26 -0300

    install antd, and create Main and Results

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.png';
 import './App.css';
-
+import { Button } from 'antd';
+import Main from './components/Main'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <Main />
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <Button type="primary">Primary</Button>
       </header>
     </div>
   );
